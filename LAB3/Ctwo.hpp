fa4347240989d23a5b161070853e7338805cd58f
wemik2323 <96520642+wemik2323@users.noreply.github.com>
2022-10-31 03:04:30 +0300

    Create Ctwo.hpp

---
+++ LAB3/Ctwo.hpp
@@ -0,0 +1,30 @@
+#ifndef PROJECTS_CTWO_H
+#define PROJECTS_CTWO_H
+#include "Cone.h"
+
+class Ctwo {
+private:
+    double d;
+    Cone* p;
+
+public:
+    Ctwo() : d{0}, p{new Cone} {}
+
+    Ctwo(double a, Cone& A) : d{a}, p{new Cone{A.getd(), A.gets_()}} {}
+
+    Ctwo(const Ctwo& A) : d{A.d} {//p{new Cone{A.p->d, A.p->s} {}
+        p = new Cone;
+        p->d = A.p->d;
+        p->s = A.p->s;
+    }
+
+    void print() {
+        std::cout << "CLASS TWO\n";
+        std::cout << "double = " << d << '\n';
+        p->print();
+    }
+
+    double getd() { return d; }
+    Cone* getCone() { return p; }
+};
+#endif
